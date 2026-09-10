8faf726f692d571897a8c688b5c34afef70ef6e8
abi15punkte <abi.15.punkte@gmail.com>
2026-09-10 21:44:24 +0200

    Refresh PWA cache after landscape layout rollback

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "einmaleins-v8";
+const CACHE_NAME = "einmaleins-v9";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./einmaleins-icon.svg", "./src/responsive.css", "./src/laptop.css"];
 
 self.addEventListener("install", (event) => {
