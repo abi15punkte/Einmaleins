8bb8ce29f1395c569568f0a10dadfa333c1f8299
abi15punkte <abi.15.punkte@gmail.com>
2026-09-10 19:48:34 +0200

    Refresh PWA cache after responsive layout update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "einmaleins-v2";
+const CACHE_NAME = "einmaleins-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./einmaleins-icon.svg"];
 
 self.addEventListener("install", (event) => {
