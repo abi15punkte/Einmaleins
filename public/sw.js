a0b327587f72081311f9887e61ad06d10fd35623
abi15punkte <abi.15.punkte@gmail.com>
2026-09-10 19:30:35 +0200

    Refresh PWA cache after app updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "einmaleins-v1";
+const CACHE_NAME = "einmaleins-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./einmaleins-icon.svg"];
 
 self.addEventListener("install", (event) => {
