4e24e599013b3a3e53b36f58cd16394d93328c6e
abi15punkte <abi.15.punkte@gmail.com>
2026-09-10 21:33:22 +0200

    Refresh cache after shared landscape layout changes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "einmaleins-v6";
+const CACHE_NAME = "einmaleins-v7";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./einmaleins-icon.svg", "./src/responsive.css", "./src/laptop.css"];
 
 self.addEventListener("install", (event) => {
