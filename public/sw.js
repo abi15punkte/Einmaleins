7311076664909dd1f427f1ba79104a91def4cb3a
abi15punkte <abi.15.punkte@gmail.com>
2026-09-14 13:40:02 +0200

    Restore original highscore navigation handling

--- public/sw.js
+++ public/sw.js
@@ -78,7 +78,6 @@ self.addEventListener("fetch", (event) => {
 
   const requestUrl = new URL(event.request.url);
   const isNavigation = event.request.mode === "navigate" || requestUrl.pathname.endsWith("/index.html");
-  const isHighscoreNavigation = event.request.mode === "navigate" && requestUrl.pathname.endsWith("/highscore.html");
   const isHighscoreAsset = requestUrl.origin === self.location.origin && HIGHSCORE_ASSETS.some((asset) => {
     const assetUrl = new URL(asset, self.location.href);
     return requestUrl.pathname === assetUrl.pathname;
@@ -95,21 +94,6 @@ self.addEventListener("fetch", (event) => {
           return response;
         })
         .catch(() => caches.match(event.request))
-    );
-    return;
-  }
-
-  if (isHighscoreNavigation) {
-    event.respondWith(
-      fetch(event.request, { cache: "no-store" })
-        .then((response) => {
-          if (response.ok) {
-            const copy = response.clone();
-            void caches.open(CACHE_NAME).then((cache) => cache.put("./highscore.html", copy));
-          }
-          return response;
-        })
-        .catch(() => caches.match("./highscore.html"))
     );
     return;
   }
