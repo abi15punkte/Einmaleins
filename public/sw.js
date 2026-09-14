3801c8b94d8959c7ad0e1e857bcbac5f77346565
abi15punkte <abi.15.punkte@gmail.com>
2026-09-14 12:55:28 +0200

    Prevent service worker from taking over the current page during startup test

--- public/sw.js
+++ public/sw.js
@@ -57,7 +57,6 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
