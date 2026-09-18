415ef75bc0d7a5220a3c94769e39ad36d2875f85
abi15punkte <abi.15.punkte@gmail.com>
2026-09-18 08:33:25 +0200

    Lehreransicht: HTML-Seite in Vite-Build aufnehmen

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   base: "/Einmaleins/",
 
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        highscore: "highscore.html",
+        lehrerHighscore: "lehrer-highscore.html"
+      }
+    }
+  },
+
   test: {
     environment: "node",
     globals: true
