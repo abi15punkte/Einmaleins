72feaf91797d02e19eaaaf579adfaa1b69ed26d8
abi15punkte <abi.15.punkte@gmail.com>
2026-09-20 09:20:15 +0200

    Restore P16 service worker asset

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@ const STARTUP_IMAGE_ASSETS = [
   "./Querformathinweis.png",
   "./Background.png",
   "./Alien.png",
-  ...Array.from({ length: 15 }, (_, index) => `./P${index + 1}.png`),
+  ...Array.from({ length: 16 }, (_, index) => `./P${index + 1}.png`),
   ...Array.from({ length: 16 }, (_, index) => `./M${index + 1}.png`),
   "./Stern1.png",
   "./Stern2.png",
